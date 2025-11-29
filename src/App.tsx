61f50be86fa265c8a0b98156e1733772e0f01cbd
gurusaiss <gurusaiss2005@gmail.com>
2025-11-30 00:13:35 +0530

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import GridBalancer from './pages/GridBalancer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
